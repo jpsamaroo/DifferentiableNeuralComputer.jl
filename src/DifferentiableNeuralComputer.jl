37a78ac091a230859f00435dc1323b70eb58e5e6
Jules Merckx <julesmerckx12@gmail.com>
2019-08-05 15:21:32 +0200

    implement lstm as in paper

--- src/DifferentiableNeuralComputer.jl
+++ src/DifferentiableNeuralComputer.jl
@@ -3,6 +3,8 @@ module DifferentiableNeuralComputer
 import Flux
 import Zygote
 
+include("src/lstm.jl")
+
 struct DNC end
 
 function (dnc::DNC)(x)
---
+++ src/lstm.jl
@@ -0,0 +1,40 @@
+using Flux
+using Flux: glorot_uniform, @treelike, Recur
+
+mutable struct DNCLstmCell
+    Wi; Wf; Ws; Wo
+    bi; bf; bs; bo
+    h; s # These aren't used in the forward pass but need to be included for Recur.
+end
+@treelike DNCLstmCell
+
+function DNCLstmCell(in::Integer, hidden::Integer; init=glorot_uniform)
+    DNCLstmCell([init(hidden, in+2*hidden) for i in 1:4]...,
+                [zeros(hidden) for i in 1:6]...)
+end
+
+#=
+    h: hidden at previous timestep
+    s: state at previous timestep
+    h′: state at previous layer (at same timestep)
+    x: original input to the LSTM
+=#
+function(c::DNCLstmCell)((h, s), (h′, x))
+    i = σ.(c.Wi * [x; h; h′] .+ c.bi)
+    f = σ.(c.Wf * [x; h; h′] .+ c.bf)
+    s′ = f .* c.s .+ i .* tanh.(c.Ws * [x; h; h′] .+ c.bs)
+    o = σ.(c.Wo * [x; h; h′] .+ c.bo)
+    h′ = o .* tanh.(s′)
+    return((h′, s′), (h′, x))
+end
+
+Flux.hidden(m::DNCLstmCell) = (m.h, m.s)
+DNCLstm(a...; ka...) = Recur(DNCLstmCell(a...; ka...))
+
+#=
+Note: for creating a multi-layered DNCLSTM, the arguments of each layer
+    need to be compatible. In other words, the hidden state has to keep
+    the same dimensions and the input size stays constant since x gets
+    passed through each layer.
+    e.g.: Chain(DNCLSTM(16,10), DNCLSTM(16,10))
+=#
