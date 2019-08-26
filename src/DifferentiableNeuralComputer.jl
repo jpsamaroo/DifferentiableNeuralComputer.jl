b2043c96df68a4777d38745259a50eb4474f992d
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-08-27 03:51:11 +0530

    introduced interfacedisect to forward pass.

--- src/DifferentiableNeuralComputer.jl
+++ src/DifferentiableNeuralComputer.jl
@@ -26,49 +26,70 @@ struct DNC
     wrtWt        # (0, 1)^{N}
     usageVec     # (0, 1)^{N}
     precedenceWt # (0, 1)^{N}
+    numWriteHeads
+    numReadHeads
+    wordSize
 end
 
+"""
+    interfacedisect(interfaceVec, writeHead, wordSize, readHeads)
+
+Disects the interface vector obtained as the output to obtain various memory
+access controls for the DNC.
+"""
 function interfacedisect(interfaceVec, writeHeads, wordSize, readHeads)
-    demarcations = cumsum([0,                           # Starting Index
-                    readHeads*wordSize,                 # read keys
-                    readHeads,                          # read strengths
-                    writeHeads*wordSize,                # write keys
-                    writeHeads,                         # write strengths
-                    writeHeads*wordSize,                # erase vectors
-                    writeHeads*wordSize,                # write vectors
-                    readHeaads,                         # free gates
-                    writeHeads,                         # allocation gates
-                    writeHeads,                         # write gates
-                    readHeads * (1 + 2* writeHeads)     # read modes
+    demarcations = cumsum([0,                     # Starting Index
+                    readHeads*wordSize,           # read keys
+                    readHeads,                    # read strengths
+                    writeHeads*wordSize,          # write keys
+                    writeHeads,                   # write strengths
+                    writeHeads*wordSize,          # erase vectors
+                    writeHeads*wordSize,          # write vectors
+                    readHeaads,                   # free gates
+                    writeHeads,                   # allocation gates
+                    writeHeads,                   # write gates
+                    readHeads * (1 + 2writeHeads) # read modes
                     ])
 
 
-    readkeys       =          interfaceVec[demarcations[1]+1:demarcations[2]]
-    readstrengths  =  oneplus(interfaceVec[demarcations[2]+1:demarcations[3]])
-    writekeys      =          interfaceVec[demarcations[3]+1:demarcations[4]]
-    writestrengths =  oneplus(interfaceVec[demarcations[4]+1:demarcations[5]])
-    eraseVec       =        σ(interfaceVec[demarcations[5]+1:demarcations[6]])
-    writeVec       =          interfaceVec[demarcations[6]+1:demarcations[7]]
-    freeGts        =        σ(interfaceVec[demarcations[7]+1:demarcations[8]])
-    allocGt        =        σ(interfaceVec[demarcations[8]+1:demarcations[9]])
-    writeGt        =        σ(interfaceVec[demarcations[9]+1:demarcations[10]])
+    readkeys       =         interfaceVec[demarcations[1]+1:demarcations[2]]
+    readstrengths  = oneplus(interfaceVec[demarcations[2]+1:demarcations[3]])
+    writekeys      =         interfaceVec[demarcations[3]+1:demarcations[4]]
+    writestrengths = oneplus(interfaceVec[demarcations[4]+1:demarcations[5]])
+    eraseVec       =       σ(interfaceVec[demarcations[5]+1:demarcations[6]])
+    writeVec       =         interfaceVec[demarcations[6]+1:demarcations[7]]
+    freeGts        =       σ(interfaceVec[demarcations[7]+1:demarcations[8]])
+    allocGt        =       σ(interfaceVec[demarcations[8]+1:demarcations[9]])
+    writeGt        =       σ(interfaceVec[demarcations[9]+1:demarcations[10]])
     readmodes      = softmax(interfaceVec[demarcations[10]+1:demarcations[11]])
+
+    readkeys  = reshape(readkeys, readHeads, wordSize)   # RH * W
+    writekeys = reshape(writekeys, writeHeads, wordSize) # WH * W
+    eraseVec  = reshape(eraseVec, writeHeads, wordSize)  # WH * W
+    writeVec  = reshape(writeVec, writeHeads, wordSize)  # WH * W
+    readmodes = reshape(readmodes, readHeads, (1+2writeHeads))  # RH * (WH for backward + WH for forward + 1 for content lookup)
+
+    return (readkeys=readkeys, readstrengths=readstrengths, writekeys=writekeys,
+            writestrengths=writestrengths, eraseVec=eraseVec, writeVec=writeVec,
+            freeGts=freeGts, allocGts=allocGts, writeGts=writeGts, readmodes=readmodes)
 end
 
-function (dnc::DNC)(readkeys, readstrengths, wrtkey, wrtstrength, eraseVec, wrtVec, freeGt, allocGt, wrtGt, readmodes)
+function (dnc::DNC)(input)
     # dynamic memory allocation
 
-    memRetVec = prod(1 .- freeGt .* dnc.readWts)  # Memory Retention Vector = [0, 1]^{N}
+    interface = interfacedisect(input, dnc.numWriteHeads, dnc.wordSize, dnc.numReadHeads)
+
+    memRetVec = prod(1 .- interface[:freeGts] .* dnc.readWts)  # Memory Retention Vector = [0, 1]^{N}
     dnc.usageVec = (dnc.usageVec .+ dnc.wrtWt .- dnc.usageVec .* dnc.wrtWt) .* memRetVec
     freelist = sortperm(dnc.usageVec)  # Z^{N}
     allocWt = zeros(dnc.usageVec)
     @. allocWt[freelist] = (1 - dnc.usageVec[freelist]) * cumprod([1; dnc.usageVec[freelist]][1:end-1])  # (0, 1)^{N}
 
     # writing
-    wrtcntWt = memprobdistrib(dnc.MemMat, wrtkey, wrtstrength) # Write content weighting = (0, 1)^{N}
-    dnc.wrtWt .= wrtGt * (allocGt * allocWt + (1 - allocGt)*wrtcntWt)
-    @. dnc.MemMat *= (ones(dnc.MemMat) - dnc.wrtWt*eraseVec') #  First we erase...
-    @. dnc.MemMat += dnc.wrtWt*wrtVec' # Then we write.
+    wrtcntWt = memprobdistrib(dnc.MemMat, interface[:writekeys], interface[:writestrengths]) # Write content weighting = (0, 1)^{N}
+    dnc.wrtWt .= interface[:writeGts] * (interface[:allocGts] * allocWt + (1 - interface[:allocGts])*wrtcntWt)
+    @. dnc.MemMat *= (ones(dnc.MemMat) - dnc.wrtWt*interface[:eraseVec]') #  First we erase...
+    @. dnc.MemMat += dnc.wrtWt*interface[:writeVec]' # Then we write.
 
     # temporal linkage
     eye = Matrix{Float32}(I, size(dnc.LinkMat)...)
@@ -83,7 +104,7 @@ function (dnc::DNC)(readkeys, readstrengths, wrtkey, wrtstrength, eraseVec, wrtV
     backwardWts = [dnc.LinkMat' * readWt for readWt in dnc.readWts]
     readcntWts = memprobdistrib.([dnc.MemMat], readkeys, readstrengths) # Read content weightings
 
-    dnc.readWts = [π[1].*b .+ π[2].*readcntWts .+ π[3].*f for (π, b, f) in (readmodes, backwardWts, forwardWts)]
+    dnc.readWts = [π[1].*b .+ π[2].*readcntWts .+ π[3].*f for (π, b, f) in (interface[:readmodes], backwardWts, forwardWts)]
     readvecs = [dnc.MemMat' * W_r for W_r in dnc.readWts]
 
     return(readvecs)
