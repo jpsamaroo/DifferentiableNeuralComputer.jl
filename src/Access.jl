3f2b096bbd0e9552ba6eb173ab6a3136c6d3d296
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-09-01 04:34:34 +0530

    removed writeHeads for the time being

--- src/Access.jl
+++ src/Access.jl
@@ -1,4 +1,5 @@
 using Flux: softmax
+using LinearAlgebra: norm
 
 cosine_sim(u, v) = (u'v)/(norm(u)*norm(v))
 
@@ -9,7 +10,7 @@ cosine_sim(u, v) = (u'v)/(norm(u)*norm(v))
 Defines a normalized probability distribution over the memory locations.
 """
 function memprobdistrib(M, k, β)
-    out = [cosine_sim(k, M[i, :]) for i in 1:size(M, 1)] .* β
+    out = [cosine_sim(k, M[:, i])[1] for i in 1:size(M, 2)] .* β
     out = softmax(out)
 end
 
@@ -18,36 +19,42 @@ oneplus(x::AbstractVecOrMat) = log.(exp.(x) .+ 1)
 mutable struct Access
     MemMat       # R^{N*W}
     LinkMat      # R^{N*N}
-    readWts      # RH element Array of [0, 1]^N
-    wrtWt        # WH element Array of (0, 1)^{N}
+
+    readWts      # RH element Array of [0, 1]^N    # Find and update its grads
+    wrtWt        # WH element Array of (0, 1)^{N}  # Find and update its grads
+
+
     usageVec     # (0, 1)^{N}
     precedenceWt # (0, 1)^{N}
     readVecs
-    numWriteHeads
+    #numWriteHeads
     numReadHeads
     wordSize
     memorySize
 end
 
-function Access(memorySize=128, wordSize=20, numReadHeads=1, numWriteHeads=1)
-    MemMat = zeros(Float32, memorySize, wordSize)
+function Access(memorySize=128, wordSize=20, numReadHeads=1)
+    MemMat = zeros(Float32, wordSize, memorySize)
+    fill!(MemMat, 1f-6)
+
     LinkMat = zeros(Float32, memorySize, memorySize)
 
     readWts = rand(Float32, memorySize, numReadHeads)
     fill!(readWts, 1f-6)
 
-    wrtWt = rand(Float32, memorySize, numWriteHeads)
+    wrtWt = rand(Float32, memorySize)
     fill!(wrtWt, 1f-6)
 
-    usageVec = zeros(Float32, memorySize, numWriteHeads)
+    usageVec = zeros(Float32, memorySize)
     fill!(usageVec, 1f-6)
+
     precedenceWt = zeros(Float32, memorySize)
 
     readVecs = zeros(Float32, wordSize, numReadHeads)
     fill!(readVecs, 1f-6)
 
     Access(MemMat, LinkMat, readWts, wrtWt, usageVec, precedenceWt, readVecs,
-            numWriteHeads, numReadHeads, wordSize, memorySize)
+            numReadHeads, wordSize, memorySize)
 end
 
 """
@@ -56,18 +63,18 @@ end
 Disects the interface vector obtained as the output to obtain various memory
 access controls for the DNC.
 """
-function interfacedisect(interfaceVec, writeHeads, wordSize, readHeads)
+function interfacedisect(interfaceVec, wordSize=16, readHeads=4)
     demarcations = cumsum([0,                     # Starting Index
                     readHeads*wordSize,           # read keys
                     readHeads,                    # read strengths
-                    writeHeads*wordSize,          # write keys
-                    writeHeads,                   # write strengths
-                    writeHeads*wordSize,          # erase vectors
-                    writeHeads*wordSize,          # write vectors
+                    wordSize,                     # write keys
+                    1,                   # write strengths
+                    wordSize,          # erase vectors
+                    wordSize,          # write vectors
                     readHeads,                    # free gates
-                    writeHeads,                   # allocation gates
-                    writeHeads,                   # write gates
-                    readHeads * (1 + 2writeHeads) # read modes
+                    1,                   # allocation gates
+                    1,                   # write gates
+                    readHeads * (1 + 2) # read modes
                     ])
 
 
@@ -82,49 +89,57 @@ function interfacedisect(interfaceVec, writeHeads, wordSize, readHeads)
     writeGt        =       σ.(interfaceVec[demarcations[9]+1:demarcations[10]])
     readmodes      = softmax(interfaceVec[demarcations[10]+1:demarcations[11]])
 
-    readkeys  = reshape(readkeys, wordSize, readHeads)   # W * RH
-    writekeys = reshape(writekeys, wordSize, writeHeads) # W * WH
-    eraseVec  = reshape(eraseVec, wordSize, writeHeads)  # W * WH
-    writeVec  = reshape(writeVec, wordSize, writeHeads)  # W * WH
-    readmodes = reshape(readmodes, (1+2writeHeads), readHeads)  # (WH for backward + WH for forward + 1 for content lookup) * RH
+    readkeys  = reshape(readkeys, wordSize, readHeads)    # RH * W
+    #writekeys = reshape(writekeys, wordSize, writeHeads) # W * WH
+    #eraseVec  = reshape(eraseVec, wordSize, writeHeads)  # W * WH
+    #writeVec  = reshape(writeVec, wordSize, writeHeads)  # W * WH
+    readmodes = reshape(readmodes, readHeads, (1+2))  # (WH for backward + WH for forward + 1 for content lookup) * RH
+
 
     return (readkeys=readkeys, readstrengths=readstrengths, writekeys=writekeys,
             writestrengths=writestrengths, eraseVec=eraseVec, writeVec=writeVec,
             freeGts=freeGts, allocGts=allocGt, writeGts=writeGt, readmodes=readmodes)
 end
 
-function (access::Access)(interfaceVec)
+function (access::Access)(interfaceVec::AbstractArray)
     # dynamic memory allocation
 
-    interface = interfacedisect(interfaceVec, access.numWriteHeads, access.wordSize, access.numReadHeads)
+    interface = interfacedisect(interfaceVec, access.wordSize, access.numReadHeads)
 
     memRetVec = prod(1 .- interface[:freeGts]' .* access.readWts, dims=2)  # Memory Retention Vector = [0, 1]^{N}
     access.usageVec = (access.usageVec .+ access.wrtWt .- access.usageVec .* access.wrtWt) .* memRetVec
-    freelist = sortperm(access.usageVec)  # Z^{N}
-    allocWt = zeros(access.usageVec)
-    @. allocWt[freelist] = (1 - access.usageVec[freelist]) * cumprod([1; access.usageVec[freelist]][1:end-1])  # (0, 1)^{N}
+
+
+    ## Generalize it for the multiple write heads
+    allocWt = zero(access.usageVec)
+    freelist = sortperm(access.usageVec[:, 1])  # Z^{N}
+    allocWt[freelist] = (1 .- access.usageVec[freelist]) .* cumprod([1; access.usageVec[freelist]][1:end-1])  # (0, 1)^{N}
 
     # writing
     wrtcntWt = memprobdistrib(access.MemMat, interface[:writekeys], interface[:writestrengths]) # Write content weighting = (0, 1)^{N}
-    access.wrtWt .= interface[:writeGts] * (interface[:allocGts] * allocWt + (1 - interface[:allocGts])*wrtcntWt)
-    @. access.MemMat *= (ones(access.MemMat) - access.wrtWt*interface[:eraseVec]') #  First we erase...
-    @. access.MemMat += access.wrtWt*interface[:writeVec]' # Then we write.
+    access.wrtWt = @. interface[:writeGts] * (allocWt * interface[:allocGts] + (1 - interface[:allocGts])' * wrtcntWt)
+    access.MemMat .*= ones(Float32, size(access.MemMat)) - access.wrtWt*interface[:eraseVec]' #  First we erase...
+    access.MemMat += access.wrtWt*interface[:writeVec]' # Then we write.
 
     # temporal linkage
-    eye = Matrix{Float32}(I, size(access.LinkMat)...)
-    prevlinkscale = @. 1 - access.wrtWt - access.wrtWt'
+    eye = one(access.LinkMat)
+    prevlinkscale = @. 1 - access.wrtWt - access.wrtWt' ## Have to do something here for multiple write heads...
     newlink = @. access.wrtWt * access.precedenceWt'
     @. access.LinkMat = (1 - eye) * (prevlinkscale * access.LinkMat + newlink)
 
     access.precedenceWt = (1 - sum(access.wrtWt)) .* access.precedenceWt .+ access.wrtWt
 
     # reading
-    forwardWts = [access.LinkMat * readWt for readWt in access.readWts]
-    backwardWts = [access.LinkMat' * readWt for readWt in access.readWts]
-    readcntWts = memprobdistrib.([access.MemMat], readkeys, readstrengths) # Read content weightings
+    forwardWts = access.LinkMat * access.readWts
+    backwardWts = access.LinkMat' * access.readWts
 
-    access.readWts = [π[1].*b .+ π[2].*readcntWts .+ π[3].*f for (π, b, f) in (interface[:readmodes], backwardWts, forwardWts)]
-    access.readvecs = [access.MemMat' * W_r for W_r in access.readWts]
+    ## make this pretty
+    readcntWts = [memprobdistrib(access.MemMat, interface[:readkeys][:, i], interface[:readstrengths][i]) for i=1:access.numReadHeads] # Read content weightings
+    readcntWts = hcat(readcntWts...)
 
-    return(readvecs)
+    ## Make this pretty...
+    access.readWts = [interface[:readmodes][i, 1].*backwardWts[:, i] .+ interface[:readmodes][i, 2].*readcntWts[:, i] .+ interface[:readmodes][i, 3].*forwardWts[:, i] for i=1:access.numReadHeads]
+    access.readWts = hcat(access.readWts...)
+    access.readVecs = access.MemMat * access.readWts
+    return(access.readVecs)
 end
--- src/DNC.jl
+++ src/DNC.jl
@@ -5,9 +5,11 @@ using Distributions: TruncatedNormal
 using Flux
 
 mutable struct DNC
-    controller
+    controller # Find and update its grad
     access::Access
-    interfaceVec
+    interfaceVecWts::Array{Float32, T} where T # Find and update its grad
+    outputWts::Array{Float32, } # Find and update its grad
+    readVecWts # Find and update its grad
 end
 
 function DNC(memory_size=16, word_size=16, num_read_heads=4, num_write_heads=1,
@@ -15,7 +17,7 @@ function DNC(memory_size=16, word_size=16, num_read_heads=4, num_write_heads=1,
     controller_input_size = input_size + word_size * num_read_heads
 
     # A truncated normal distribution with no elements further than 2σ of μ.
-    dist = TruncatedNormal(0, 0.01, -0.02, 0.02)
+    dist = TruncatedNormal(0, 0.1, -0.2, 0.2)
 
     # The interface vector of this dimension will only work when the number of
     # write heads is equal to 1. I have yet to figure out how this value changes
@@ -23,22 +25,31 @@ function DNC(memory_size=16, word_size=16, num_read_heads=4, num_write_heads=1,
     interface_vec_dimensions = word_size * num_read_heads + 3word_size + 5num_read_heads + 3
     controller_output_size = output_size + interface_vec_dimensions
     controller = LSTM(controller_input_size, controller_output_size)
-    access = Access(memory_size, word_size, num_read_heads, num_write_heads)
+    access = Access(memory_size, word_size, num_read_heads)
 
-    interface_vec = rand(dist, interface_vec_dimensions)
-    DNC(controller, access, interface_vec)
+    interfaceVecWts = Float32.(rand(dist, interface_vec_dimensions, controller_output_size))
+    outputWts = Float32.(rand(dist, output_size, controller_output_size))
+    readVecWts = Float32.(rand(dist, output_size, num_read_heads*word_size))
+    DNC(controller, access, interfaceVecWts, outputWts, readVecWts)
 end
 
-function (dnc::DNC)(input)
+function (dnc::DNC)(input::AbstractArray)
     readVecs = dnc.access.readVecs
 
     # flattening readVecs
-    readVecs = reshape(readVecs, readVecs |> size |> prod)
+    readVecs = reshape(readVecs, length(readVecs))
 
     # concatinating them with input to form controller input
     controller_input = [input; readVecs]
 
     controller_output = dnc.controller(controller_input)
 
+    interfaceVec = dnc.interfaceVecWts * controller_output
+    output = dnc.outputWts * controller_output
 
+    readvecs = dnc.access(interfaceVec)
+    readvecs = reshape(readvecs, length(readvecs))
+    readvecs = dnc.readVecWts * readvecs
+
+    output + readvecs
 end
